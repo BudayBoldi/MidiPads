17395848373309c26633a1e44c33722819aa7b96
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2025-04-12 20:24:25 +0200

    Update midi.py
    now i have volume control

--- midi.py
+++ midi.py
@@ -4,26 +4,24 @@ with mido.open_input("Oxygen Pro Mini Mackie/HUI") as inport:
     for msg in inport:
         if msg.type == "note_on":
             print(msg)
+            vol = msg.velocity / 100
             match msg.note:
                 case 40:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./Kick.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./Kick.wav"], stdout=subprocess.PIPE)
                 case 41:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./Kick2.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./Kick2.wav"], stdout=subprocess.PIPE)
                 case 42:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./Kick3.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./Kick3.wav"], stdout=subprocess.PIPE)
                 case 43:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./Snare.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./Snare.wav"], stdout=subprocess.PIPE)
                 case 48:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./HHat.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./HHat.wav"], stdout=subprocess.PIPE)
                 case 49:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./HHat2.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./HHat2.wav"], stdout=subprocess.PIPE)
                 case 50:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./Crash.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./Crash.wav"], stdout=subprocess.PIPE)
                 case 51:
-                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "./Crash2.wav"], stdout=subprocess.PIPE)
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "volume=" + str(vol), "./Crash2.wav"], stdout=subprocess.PIPE)
                 case _:
                     print("ERROR!")
-                
-            
-        
-    
+
