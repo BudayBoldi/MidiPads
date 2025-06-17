f32a08611324f0fe331917c4b97a1080404f2d10
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2025-06-17 20:24:45 +0200

    Add files via upload
    lazy ahh code ...

---
+++ midikeys.py
@@ -0,0 +1,14 @@
+import mido, subprocess
+
+with mido.open_input("Oxygen Pro Mini USB MIDI") as inport:
+    for msg in inport:
+        if msg.type == "note_on":
+            print(msg)
+            vol = msg.velocity / 100
+            for i in range(73):
+                if msg.note == i:
+                    subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "aformat=channel_layouts=stereo,asetrate=44100*" + str(i) + "/50,atempo=50/" + str(i) + ",volume=" + str(vol), "./Sample.wav"], stdout=subprocess.PIPE)
+                
+            
+        
+    
