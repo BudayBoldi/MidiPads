0698451f229cb28d2ce86eff46782d35e48a3d85
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2025-07-18 03:12:25 +0200

    Update midikeys.py
    fix

--- midikeys.py
+++ midikeys.py
@@ -1,10 +1,17 @@
 import mido, subprocess
 
+lista = []
+
 with mido.open_input("Oxygen Pro Mini USB MIDI") as inport:
     for msg in inport:
         if msg.type == "note_on":
             print(msg)
             vol = msg.velocity / 100
-            subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "aformat=channel_layouts=stereo,asetrate=44100*" + str(msg.note) + "/50,atempo=50/" + str(msg.note) + ",volume=" + str(vol), "./Sample.wav"], stdout=subprocess.PIPE)
+            new = subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-af", "aformat=channel_layouts=stereo,asetrate=44100*" + str(msg.note) + "/50,atempo=50/" + str(msg.note) + ",volume=" + str(vol), "./Sample.wav"], stdout=subprocess.PIPE)
+            lista.append(new)
+        else:
+            for l in lista:
+                l.kill()
+            
         
     
